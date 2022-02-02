8145532d08ab9914a2d1117db3a4d3db71482f95
Scanvir <vitaliy@skalnyy.com>
2022-02-02 08:38:25 +0200

    cookie
    cookie

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,10 +9,11 @@
             <li>Вхід (Ваша пошта: <?php print_r($data[0]['email']); ?>, ваше і`мя: <?php print_r($data[0]['personName']); ?>)</li>
             <li><a href="/logout">Вихід</a></li>
             <li><a href="/resetpassword">Відновлення пароля</a></li>
+            <li><a href="/ololo">Сторінка 404</a></li>
         </ul>
         Не реализовано:
         <ul>
-            <li>Реєстрація з підтвердженням пошти</li>
+            <li><a href="/register">Реєстрація з підтвердженням пошти</a></li>
             <li>Редагування профіля</li>
         </ul>
     </p>
